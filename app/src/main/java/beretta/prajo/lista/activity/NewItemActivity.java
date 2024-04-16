8e3040cee473d956fb3da7dae3049c3d34095c2d
prajog <gabrielapberetta@gmail.com>
2024-04-16 11:12:20 -0300

    Continuação da app Lista

--- app/src/main/java/beretta/prajo/lista/activity/NewItemActivity.java
+++ app/src/main/java/beretta/prajo/lista/activity/NewItemActivity.java
@@ -1,12 +1,18 @@
 package beretta.prajo.lista.activity;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import beretta.prajo.lista.R;
 
@@ -30,6 +36,52 @@ public class NewItemActivity extends AppCompatActivity {
             }
         });
 
+        Button btnAddItem = findViewById(R.id.btnAddItem);
+
+        btnAddItem.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (photoSelected == null) {
+                    Toast.makeText(NewItemActivity.this, "É necessário selecionar uma imagem!", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etTitle = findViewById(R.id.etTitle);
+                String title = etTitle.getText().toString();
+                if (title.isEmpty()){
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir um título", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                EditText etDesc = findViewById(R.id.etDesc);
+                String desc = etDesc.getText().toString();
+                if (desc.isEmpty()){
+                    Toast.makeText(NewItemActivity.this, "É necessário inserir uma descrição", Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+                Intent i = new Intent();
+                i.setData(photoSelected);
+                i.putExtra("title", title);
+                i.putExtra("description", desc);
+                setResult(Activity.RESULT_OK, i);
+                finish();
+            }
+        });
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == PHOTO_PICKER_REQUEST){
+            if(resultCode == Activity.RESULT_OK){
+                photoSelected = data.getData();
+                ImageView imvfotoPreview = findViewById(R.id.imvPhotoPreview);
+                imvfotoPreview.setImageURI(photoSelected);
+            }
+        }
+    }
+
+
 }
