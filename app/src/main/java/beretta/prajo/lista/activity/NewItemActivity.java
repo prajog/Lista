93f177674ce87ce1fdeecc2ed0603b55ab8d5e7e
prajog <gabrielapberetta@gmail.com>
2024-04-16 10:51:18 -0300

    ab

--- app/src/main/java/beretta/prajo/lista/activity/NewItemActivity.java
+++ app/src/main/java/beretta/prajo/lista/activity/NewItemActivity.java
@@ -2,15 +2,34 @@ package beretta.prajo.lista.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 import beretta.prajo.lista.R;
 
 public class NewItemActivity extends AppCompatActivity {
+    static int PHOTO_PICKER_REQUEST = 1;
+    Uri photoSelected = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_item);
+
+
+        ImageButton imgCI = findViewById(R.id.imbCl);
+        imgCI.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent photoPickerIntent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                photoPickerIntent.setType("image/*");
+                startActivityForResult(photoPickerIntent, PHOTO_PICKER_REQUEST);
+            }
+        });
+
+
     }
 }
